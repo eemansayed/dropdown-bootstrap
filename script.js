cd04337f736f969bdeb88bff8d62891a276516f4
Eeman Sayed <eeman.sayed.12@gmail.com>
2022-06-04 14:24:46 +0100

    add styles

---
+++ script.js
@@ -0,0 +1,10 @@
+const featureBox = document.querySelector(".features-box");
+const icon = document.querySelector(".features-box .icon svg");
+const productInfoHeading = document.querySelector(".product-info h1");
+const productInfoParagraph = document.querySelector(".product-info p");
+
+featureBox.addEventListener("mouseover", (event) => {
+  icon.style.color = "#0d6efd";
+  productInfoHeading.style.color = "#0d6efd";
+  productInfoParagraph.style.color = "#0d6efd";
+});
